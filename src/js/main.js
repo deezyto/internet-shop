e9bda9c5fb72b40e3fd139297e3d754263fc8adb
Andrey <asvidovskiy@gmail.com>
2022-08-23 13:27:50 +0300

    sidebar complate

--- src/js/main.js
+++ src/js/main.js
@@ -22,4 +22,56 @@ window.addEventListener('DOMContentLoaded', () => {
     const cloneNode = item.cloneNode(true);
     item.parentNode.append(cloneNode);
   }
+
+  const menuButton = document.querySelectorAll('.category-parent');
+  const categoryLink = document.querySelector('.category-link');
+
+  
+  function hideMenuLink() {
+    menuButton.forEach((button, index) => {
+      if (index === 0) {
+        button.classList.add('active');
+      } else {
+        button.classList.add('hide');
+      }
+     
+      if (button.nextSibling) {
+        if (button.nextSibling.className === 'category-link') {
+          button.nextSibling.classList.add('hide');
+        }
+      }
+    });
+  }
+
+  function showMenuLink(index, e) {
+    try {
+      menuButton[index + 1].classList.toggle('hide');
+    } catch {}
+
+    /* if (e.nextSibling) {
+      if (e.nextSibling.className === 'category-link') {
+        e.nextSibling.classList.remove('hide');
+      }
+    } */
+    console.log(menuButton[index].parentNode.parentNode.children)
+    if (menuButton[index].nextSibling) {
+      if (menuButton[index].nextSibling.classList.contains('category-link')) {
+        menuButton[index].nextSibling.classList.toggle('hide');
+      }
+    }
+  }
+
+  function triggersMenu() {
+    menuButton.forEach((button, index) => {
+      button.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(e.target)
+        showMenuLink(index, e.target);
+      })
+    });
+  }
+
+  hideMenuLink();
+  triggersMenu();
+  
 });
